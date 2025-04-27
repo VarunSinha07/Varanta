c0f3c0b84e0f4bb7a6942ce324246eadbe94641e
VarunSinha07 <varunsinha2604@gmail.com>
2025-04-27 18:17:14 +0530

    try3

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
+  
 };
 
 export default nextConfig;
