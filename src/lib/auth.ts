0b6b3130558b2c8216b24f330bbf77abfddd0cbd
VarunSinha07 <varunsinha2604@gmail.com>
2025-04-27 18:24:07 +0530

    try4

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,11 +2,17 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
 
- 
-const prisma = new PrismaClient();
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
+        provider: "postgresql",
     }),
     emailAndPassword: {  
         enabled: true
